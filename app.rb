23948af08760fce2713b08ccdfa13fde83ab20f1
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-19 23:34:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,14 +19,14 @@ class App < Sinatra::Base
     "Goodbye, #{@name}."
   end
   
-  #   get "/multiply/:num1/:num2" do
-  #   @product = params[:num1.to_i] * params[:num2.to_i]
-  #   "#{@product}"
-  # end
-  
     get "/multiply/:num1/:num2" do
-    @product = params[:num1].to_i * params[:num2].to_i
+    @product = params[:num1.to_i] * params[:num2.to_i]
     "#{@product}"
   end
+  
+  #   get "/multiply/:num1/:num2" do
+  #   @product = params[:num1].to_i * params[:num2].to_i
+  #   "#{@product}"
+  # end
   
 end
