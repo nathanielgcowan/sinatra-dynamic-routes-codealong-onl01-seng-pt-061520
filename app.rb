f824074dd85a18a96c3e80a3b3fbdbc268930e6c
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-19 23:31:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ class App < Sinatra::Base
   end
   
     get "/multiply/:num1/:num2" do
-    @user_name = params[:num1.to_i]
+    @product = params[:num1.to_i] * params[:num1.to_i]
     "Hello #{@user_name}!"
   end
 end
